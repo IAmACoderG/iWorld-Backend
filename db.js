68083f684bc75408ecf62bf971acaf6da8a4bf66
Gaurav <guddugauravpp25@yahoo.co.uk>
2024-04-05 12:02:07 +0530

    some comment removed

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-//working
 const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
@@ -13,42 +12,3 @@ const connectToMongo = async () => {
 };
 
 module.exports = connectToMongo;
-
-
-// //not working
-// const mongoose = require('mongoose');
-// const mongoURI = 'mongodb://localhost:27017';
-
-
-
-// const connectToMongo = async () => {
-//     try {
-//         await mongoose.connect(mongoURI)
-
-//         console.log("connect to mongo successfully");
-
-//     } catch (err) {
-//         console.log("failed to connect", err)
-//     }
-// };
-//module.exports = connectToMongo;
-
-
-
-
-
-//working
-// const mongoose = require('mongoose');
-// const server = '127.0.0.1:27017';
-
-// const connectToMongo = async () => {
-//     try {
-//         await mongoose.connect(`mongodb://${server}`);
-
-//         console.log("mongo connected");
-//     }catch(err){
-//         console.log('failed to connected mongo',err)
-//     }
-
-//     };
-// module.exports = connectToMongo;
