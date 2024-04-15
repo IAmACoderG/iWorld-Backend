982e63e2a3dc1e7351e72fc3a69a6bcb6d93b983
Gaurav <guddugauravpp25@yahoo.co.uk>
2024-04-15 16:41:32 +0530

    cors added

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT || 4000
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    Credential: true
+}))
 
 //Available routes
 app.use('/api/auth', require('./routes/auth'));
